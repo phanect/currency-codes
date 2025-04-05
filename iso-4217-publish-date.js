cb6b79428e10d340064ec749feb9a9b3baaf957b
Jumpei Ogawa <git@phanective.org>
2025-04-06 08:43:02 +0900

    chore: `npm run iso`

--- iso-4217-publish-date.js
+++ iso-4217-publish-date.js
@@ -1,7 +1,7 @@
 /*
 	Follows ISO 4217, https://www.iso.org/iso-4217-currency-codes.html
 	See https://www.currency-iso.org/dam/downloads/lists/list_one.xml
-	Data last updated 2024-06-25
+	Data last updated 2025-03-31
 */
 
-module.exports = "2024-06-25";
+module.exports = "2025-03-31";
